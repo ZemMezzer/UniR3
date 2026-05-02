ca2c0254c141d272aa487bd15541dea562ef5fbb
ZemMezzer <drstive121319@gmail.com>
2026-05-02 15:42:54 +0200

    remove original r3 from the project

--- R3/ConnectableObservable.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace R3;
-
-public abstract class ConnectableObservable<T> : Observable<T>
-{
-    public abstract IDisposable Connect();
-}
--- R3/Internal/TimeSpanExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace R3.Internal;
-
-internal static class TimeSpanExtensions
-{
-    public static TimeSpan Normalize(this TimeSpan timeSpan)
-    {
-        return timeSpan >= TimeSpan.Zero ? timeSpan : TimeSpan.Zero;
-    }
-}
